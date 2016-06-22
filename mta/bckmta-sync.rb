6ccc7afba7990edd31f647d57406980539a78745
Carlos Lapao <cjlapao@gmail.com>
2016-06-22 14:20:44 +0100

    MTA Sync ver 0.2

--- mta/bckmta-sync.rb
+++ mta/bckmta-sync.rb
@@ -3,26 +3,133 @@
 #
 #
 #author: Carlos Lapao
-#Ver: 0.0.0.100
+#Ver: 0.2.0.000
 #ITTECH24.co.uk
 #all rights reserved
-class MtaSync
-	def initialize
-		IO.popen("rsync -avz --delete /var/mail/virtual/ root@mailbck:/var/mail/virtual"){|_io|
-			_out = _io.readlines
-			output = File.open("/root/scripts/bckmta-sync.output","a")
-			_out.each{|line|
-				output.write(line)
-			}
-			output.write("#{Time.now}########################################################\n\n")
-			output.close
-			_io.close
+
+require 'open3'
+$logdir 					= '/var/log/mta-sync.log'
+$_smtp						= '192.168.1.2'
+$_smtpuser				= 'it@alara.co.uk'
+$_smtppasswd			= 'A1ara45678'
+$_email						= 'it@alara.co.uk'
+
+class CheckError
+	attr_accessor :error,
+								:errorcode,
+	 							:lines,
+								:sent,
+								:received,
+								:total
+
+	def initialize(_stdout, _stderror, _stdsuccess)
+		@error 			= error
+		@errorcode 	= errorcode
+		@lines 			= Array.new
+		@sent 			= sent
+		@recieved 	= received
+		@total 			= total
+
+		if _stdsuccess
+			@error 			= false
+		elsif !_stdsuccess
+			@error			= true
+		end
+
+		@errorcode 	= _stderror.split(/\n/)
+		@stdout 		= _stdout.split(/\n/)
+		@sent 			= 0
+		@received 	= 0
+		@total 			= 0
+		collect(@stdout)
+	end
+
+	def collect(lines)
+		collect = false
+		lines.each{|line|
+			if line.include? "sending incremental file list"
+				collect = true
+			end
+			if (line.include? "sent ") || (line.include? "total")
+				collect = false
+			end
+			if (line =="\n") && (collect == true)
+				collect = false
+			end
+			if (collect == true) && (!line.include? "sending incremental")
+				@lines.push(line)
+			end
+				if (line.include? "sent") && (line.include? "bytes")
+				@sent = line[line.index("sent ")+4..line.index("bytes")-1]
+				tline = line[line.index("bytes")+7..line.length]
+				if !tline.nil?
+					@received = tline[tline.index("received ")+9..tline.index("bytes")-1]
+				end
+			end
+			if line.include? "total"
+				@total = line[line.index("is ")+3..line.index("speedup")-1]
+			end
 		}
 	end
 end
 
-log = File.open("/var/log/bckmta-sync.log","a")
-log.write("#{Time.now} MTA sync started\n")
+class GenerateLog
+	def initialize(pid,msg)
+		output = File.open($logdir,"a")
+		date = Time.now
+		output.write("#{date} bckmta-sync[#{pid}]: #{msg}\n")
+		output.close
+	end
+end
+
+class SendEmail
+	def initialize(_error,_errorcode)
+		begin
+			if _error
+				_subject = "[Failure] BCKMTA syncronization failed"
+				_body = "Alara Systems\n\nThe Backup MTA syncronization failed with the following output\n"
+				_errorcode.each{|line|
+					_body = "#{_body}#{line.gsub(/[^a-zA-Z0-9]+/, ' ').strip}\n"
+				}
+			end
+			IO.popen("echo '#{_body}' |"+
+				"mailx -r 'it@alara.co.uk' -s '#{_subject}' "+
+				"-S smtp='#{$_smtp}:587' "+
+				"-S smtp-use-starttls "+
+				"-S smtp-auth=login "+
+				"-S smtp-auth-user='#{$_smtpuser}' "+
+				"-S smtp-auth-password='#{$_smtppasswd}' "+
+				"-S ssl-verify=ignore "+
+				"#{$_email}"){|_io| _io.close}
+		rescue StandardError
+			puts "\e[0;31mError sending email to administrator, please review the conf file\t\t\t\t[ERROR]\e[0m"
+		end
+	end
+end
+
+
+class MtaSync
+	def initialize
+		captured_stdout = '\n'
+		captured_stderr = '\n'
+		exit_status = Open3.popen3(ENV,"rsync -avz --delete /var/mail/virtual/ root@mailbck:/var/mail/virtual") {|stdin, stdout, stderr, wait_thr|
+			@pid = wait_thr.pid
+			GenerateLog.new(@pid,"Starting syncronization")
+			stdin.close
+			@captured_stdout = stdout.read
+			@captured_stderr = stderr.read
+			wait_thr.value
+		}
+		sync = CheckError.new(@captured_stdout,@captured_stderr,exit_status.success?)
+		if !sync.error
+			GenerateLog.new(@pid,"syncronization finished sucessfully: sent #{sync.sent} bytes, received #{sync.received} bytes")
+		elsif sync.error
+			GenerateLog.new(@pid,"There was an error syncronizing the files")
+			sync.errorcode.each{|line|
+				GenerateLog.new(@pid,line)
+			}
+			SendEmail.new(sync.error,sync.errorcode)
+		end
+	end
+end
 sync = MtaSync.new
-log.write("#{Time.now} MTA sync finished\n")
-log.close
--- mta/mta-sync.rb
+++ mta/mta-sync.rb
@@ -3,13 +3,13 @@
 #
 #
 #author: Carlos Lapao
-#Ver: 0.0.0.100
+#Ver: 0.2.0.000
 #ITTECH24.co.uk
 #all rights reserved
 
 require 'open3'
 $logdir 					= '/var/log/mta-sync.log'
-$_smtp						= 'mail.alara.co.uk'
+$_smtp						= '192.168.1.2'
 $_smtpuser				= 'it@alara.co.uk'
 $_smtppasswd			= 'A1ara45678'
 $_email						= 'it@alara.co.uk'
@@ -47,25 +47,29 @@ class CheckError
 	def collect(lines)
 		collect = false
 		lines.each{|line|
-			if line.include? "sending incremental file list"
-				collect = true
-			end
-			if (line.include? "sent ") || (line.include? "total")
-				collect = false
-			end
-			if (line =="\n") && (collect == true)
-				collect = false
-			end
-			if (collect == true) && (!line.include? "sending incremental")
-				@lines.push(line)
-			end
-			if line.include? "sent"
-				@sent = line[line.index("sent ")+4..line.index("bytes")-1]
-				tline = line[line.index("bytes")+7..line.length]
-				@received = tline[tline.index("received ")+9..tline.index("bytes")-1]
-			end
-			if line.include? "total"
-				@total = line[line.index("is ")+3..line.index("speedup")-1]
+			if !line.nil?
+				if line.include? "sending incremental file list"
+					collect = true
+				end
+				if (line.include? "sent ") || (line.include? "total")
+					collect = false
+				end
+				if (line =="\n") && (collect == true)
+					collect = false
+				end
+				if (collect == true) && (!line.include? "sending incremental")
+					@lines.push(line)
+				end
+				if (line.include? "sent") && (line.include? "bytes")
+					@sent = line[line.index("sent ")+4..line.index("bytes")-1]
+					tline = line[line.index("bytes")+7..line.length]
+					if !tline.nil?
+						@received = tline[tline.index("received ")+9..tline.index("bytes")-1]
+					end
+				end
+				if line.include? "total"
+					@total = line[line.index("is ")+3..line.index("speedup")-1]
+				end
 			end
 		}
 	end
@@ -87,7 +91,7 @@ class SendEmail
 				_subject = "[Failure] MTA syncronization failed"
 				_body = "Alara Systems\n\nThe MTA syncronization failed with the following output\n"
 				_errorcode.each{|line|
-					_body = "#{_body}\n#{line}"
+					_body = "#{_body}#{line.gsub(/[^a-zA-Z0-9]+/, ' ').strip}\n"
 				}
 			end
 			IO.popen("echo '#{_body}' |"+
@@ -106,17 +110,17 @@ end
 
 class MtaSync
 	def initialize
-		captured_stdout = ''
-		captured_stderr = ''
+		captured_stdout = '\n'
+		captured_stderr = '\n'
 		exit_status = Open3.popen3(ENV,"rsync -avz --delete /var/mail/virtual/ root@backupnas:/Backups/mta-rsync") {|stdin, stdout, stderr, wait_thr|
 			@pid = wait_thr.pid
+			GenerateLog.new(@pid,"Starting syncronization")
 			stdin.close
-			captured_stdout = stdout.read
-			captured_stderr = stderr.read
+			@captured_stdout = stdout.read
+			@captured_stderr = stderr.read
 			wait_thr.value
 		}
-		GenerateLog.new(@pid,"Starting syncronization")
-		sync = CheckError.new(captured_stdout,captured_stderr,exit_status.success?)
+		sync = CheckError.new(@captured_stdout,@captured_stderr,exit_status.success?)
 		if !sync.error
 			GenerateLog.new(@pid,"syncronization finished sucessfully: sent #{sync.sent} bytes, received #{sync.received} bytes")
 		elsif sync.error
